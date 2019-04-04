77c38c868abe82b28959d2eb2a6f5923d95d2aaa
Shujun Liu <liushuj@hulk.soic.indiana.edu>
2019-04-03 23:57:00 -0400

    Assignment 4.3

---
+++ system/cqueue.c
@@ -0,0 +1,42 @@
+#include<cqueue.h>
+
+int init_queue(struct Queue* q,int maxsize){
+	if(maxsize<=0)
+		return -1;
+	q->arr=(void**)getmem(maxsize*sizeof(void*));
+	q->size=maxsize;
+	q->head=0;
+	q->tail=0;
+	q->full=0;
+	q->empty=1;
+	return 0;
+}
+
+int free_queue(struct Queue* q){
+	freemem(q->arr,q->size*sizeof(void*));
+	return 0;
+}
+
+int push_queue(struct Queue* q,void* element)
+{
+	if(q->full)
+		return -1;
+	q->arr[q->tail]=element;
+	q->empty=0;
+	q->tail=(q->tail+1)%(q->size);
+	if(q->tail==q->head)
+		q->full=1;
+	return 0;
+}
+
+int pop_queue(struct Queue* q,void** element)
+{
+	if(q->empty)
+		return -1;
+	*element=q->arr[q->head];
+	q->head=(q->head+1)%(q->size);
+	q->full=0;
+	if(q->head==q->tail)
+		q->empty=1;
+	return 0;
+}
